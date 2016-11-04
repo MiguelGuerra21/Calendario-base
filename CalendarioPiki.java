260e747421d186e0a05fd4668ad02bd8c512ad11
Miguel Guerra <migueclase2@gmail.com>
2016-11-04 13:45:00 +0100

    Calendario con 3 metodos

---
+++ CalendarioPiki.java
@@ -0,0 +1,63 @@
+
+/**
+ * Write a description of class CalendarioPiki here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalendarioPiki
+{
+    // instance variables - replace the example below with your own
+    private int dia;
+    
+    private int mes;
+    
+    private int año;
+
+    /**
+     * Constructor for objects of class CalendarioPiki
+     */
+    public CalendarioPiki()
+    {
+        // initialise instance variables
+        dia = 01;
+        mes = 01;
+        año = 01;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void introducirFecha(int d,int m,int a)
+    {
+        
+        dia = d;
+        mes = m;
+        año = a;
+    }
+    
+    /**
+     * Permita, a través de un método, avanzar en un día la fecha almacenada. El método deberá de preocuparse de que la nueva fecha del objeto sea una fecha legal teniendo en cuenta que
+     * No vamos a preocuparnos por los años bisiestos.
+     * Todos los meses van a tener para nosotros 30 días
+     */
+    public void avanzarFecha () {
+        if(dia != 30) {
+            
+        dia = dia + 1;
+        
+    }
+    else {
+        System.out.println ("Fecha no valida");
+    }
+}
+    /**
+     * metodo que devuelve la fecha actual
+     */
+    public String mostrarFecha() {
+        return (dia + "-" + mes + "-" + año);
+    }
+}
